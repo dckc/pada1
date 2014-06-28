489037e04ee0d621596354cd3668c368bc280dfd
Dan Connolly <dckc@madmode.com>
2014-06-27 22:27:12 -0500

    analyze HTML with htmlcleaner

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "pada1"
 version := "1.0"
 
 libraryDependencies += "net.databinder.dispatch" % "dispatch-core_2.10" % "0.11.1"
+
+libraryDependencies += "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.8"
--- src/main/scala/com/madmode/scgstats/Go.scala
+++ src/main/scala/com/madmode/scgstats/Go.scala
@@ -1,13 +1,25 @@
 package com.madmode.scgstats
 
+import org.htmlcleaner.HtmlCleaner
+
 /**
  * Fetch some info from the web.
  * ack: http://dispatch.databinder.net/Dispatch.html
  */
 object Go extends App {
   import dispatch._, Defaults._
-  val svc = url("http://api.hostip.info/country.php")
-  val country = Http(svc OK as.String)
-  for (c <- country)
-    println("Hi!" + c)
+
+  // http://htmlcleaner.sourceforge.net/javause.php
+  val svc = url("http://htmlcleaner.sourceforge.net/javause.php")
+  val pg = Http(svc OK as.String)
+  for (content <- pg) {
+    val cleaner = new HtmlCleaner();
+    val doc = cleaner.clean(content);
+
+    for (elem <- doc.getElementsByName("a", true)) {
+      println("anchor: " + elem.getText)
+    }
+
+    println("Done!" + content.length())
+  }
 }
