cfa0a700c1579e5657c69c00525dde89ac9fd4c5
Brennan Connolly <uberike219@gmail.com>
2014-08-16 02:09:09 -0500

    Lots of progress since last commit.
    Program now stores csv files in a public folder so that Postgres can access them, properly fetches the date, and replaces commas in player tags with semicolons so that the csv files aren't messed up.

--- src/main/scala/Extra.scala
+++ src/main/scala/Extra.scala
@@ -1,24 +1,28 @@
 /**
  * Created by Brennan on 8/2/2014.
  */
+import java.io._
+import dispatch._, Defaults._
+import dispatch.url
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 import scala.collection.JavaConverters._
 
 object Extra extends App {
 
-  import dispatch._, Defaults._
-
   // http://htmlcleaner.sourceforge.net/javause.php
-  val svc = url("http://challonge.com/dzpm6")
-  val pg = Http(svc OK as.String)
 
-  for (content <- pg) {
+  val svc = url("http://challonge.com/dzpm6")
+  val page = Http(svc OK as.String)
+
+  val content = page()
     val doc = Jsoup.parse(content)
     val allSets = doc.select("td.core").asScala
     var match_num: Int = 0
 
-    /* for (set <- allSets) {
+
+    /*
+    for (set <- allSets) {
       match_num += 1
       val setInfo = set.select(".participant-present").first()
       val dataRound = setInfo.attr("data-round")
@@ -39,12 +43,10 @@ object Extra extends App {
       val wSeed = set.select("." + winnerHalf + "_seed").text()
       val lSeed = set.select("." + loserHalf + "_seed").text()
 
-
-
-
     } */
 
-    /* def FindStartDate (tourneyAddress: String) = {
+    /*
+    def FindStartDate (tourneyAddress: String) = {
       import dispatch._, Defaults._
       import dispatch.url
 
@@ -66,20 +68,101 @@ object Extra extends App {
 
     for (dateOf <- FindStartDate("http://challonge.com/dzcscgpm3")){
       println(dateOf)
-    }*/
+    }
+    */
+
 
     val tourney = doc.select("div#title").first().text()
     val host = doc.select(".host").select("a").text()
-    val tourneyType = doc.select(".list .full-width").text().trim().split(" ")(0)
-    val entrants = tourneyType.trim().split(" ")(0)
+    // val tourneyType = doc.select(".list .full-width").text().trim().split(" ")(0)
+    val entrants = doc.select(".list .left-col").text().trim().split(" ")(0)
+    println(entrants)
 
-    println(tourneyType)
-
-
-
+    // println(tourneyType)
 
 
+    /*
+    val array1 : Array[String] = Array("first", "second", "third")
+    val array2 : Array[String] = Array("first", "second", "third")
+    val array3 : Array[String] = Array("first", "second", "third")
+    val list1 : List[Array[String]] = List(array1, array2, array3)
+
+    println("Please work " + array1(0))
+    println("Please please work " + list1(0))
+    */
+
+    /*
+    val example1 = new File("example1.csv")
+
+    val string1 = "first,second,third"
+    val string2 = "first,second,third"
+    val string3 = "first,second,third"
+    val arrayString2 = Array(string1, string2, string3)
+
+    println(arrayString2(0))
+    println(arrayString2(1))
+    println(arrayString2(2))
+
+   def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
+      val p = new java.io.PrintWriter(f)
+      try { op(p) } finally { p.close() }
+    }
+
+    val data = Array(arrayString2)
+    printToFile(example1)(p => {
+      data.foreach(p.println)
+    })
+    */
+
+    def findTourneyDate(tourneyHost : String, tourneyAddress : String) : String = {
+      var pgNumber = 1
+      var tourneyDate = ""
+      val fullTourneyAddress = "http://challonge.com/" + tourneyAddress
+      // Goal: When the "address" string matches the web address of the relevant tournament,
+      // this value becomes non-null, the corresponding date is found, and the loop ends.
+
+      while (tourneyDate.equals("")) {
+        // println(pgNumber)
+
+        val append = if (pgNumber == 1) "" else "?page=" + pgNumber
+        val serviceD = url("http://challonge.com/users/" + tourneyHost + append)
+        val pageD = Http(serviceD OK as.String)
+        // Include a check to make sure the page exists?
+
+        val contentD = pageD()
+        val docD = Jsoup.parse(contentD)
+        val allTourneys = docD.select("tr").asScala
+
+        //print(allTourneys)
+
+        for (oneTourney <- allTourneys) {
+
+          val tourneyInfo = oneTourney.select("tr")
+          if (tourneyInfo != null) {
+            val address = tourneyInfo.select("td").select("a").attr("href")
+            val date: String = tourneyInfo.select(".hidden-phone").last().text()
+            if (address.equals(fullTourneyAddress)) tourneyDate = date
+            //println(tourneyDate)
+            //println(date)
+            //println("*****")
+          }
+        }
+
+        pgNumber += 1
+
+      }
+
+      tourneyDate
+
+    }
+
+    val date =
+     findTourneyDate("kcsmashbros", "diordie")
+    // print(date)
+    // Expected result date: 12-28-13
+
+  println("Done.")
 
 
   }
-}
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@
  * Created by Brennan on 8/2/2014.
  */
 
+import java.io._
 import dispatch._, Defaults._
 import dispatch.url
 import org.jsoup.Jsoup
@@ -10,28 +11,42 @@ import scala.collection.JavaConverters._
 
 object Main extends App {
 
-  // http://htmlcleaner.sourceforge.net/javause.php
-  val svc = url("http://challonge.com/dzcscgpm3")
-  val pg = Http(svc OK as.String)
+  // **The only required input is the web address of the Challonge bracket.** \\
+  def gatherData(bracketAddress : String) = {
 
-  for (content <- pg) {
+    // http://htmlcleaner.sourceforge.net/javause.php
+    val service = url("http://challonge.com/" + bracketAddress)
+    val page = Http(service OK as.String)
+
+    val content = page()
     val doc = Jsoup.parse(content)
-    val allSets = doc.select("td.core").asScala
-    var match_num: Int = 0
+    val allSets = doc.select("td.core").asScala //each set can be found under the html tag "td.core"
 
-    val setSchema = Array("tourney", "hosted_by", "entrants", "match_num", "round_num", "w_tag", "l_tag", "w_games_won", "l_games_won", "w_seed", "l_seed")
-    // Include "date_of" column later.
+    val csvFile = new PrintWriter("C:\\Users\\Public\\CSV_Files\\" + bracketAddress + ".csv")
+
+    //METHOD - printColumnLabels()
+
+    val setSchema = Array("tourney", "hosted_by",  "date_of",  "entrants",
+      "match_num",
+      "round_num",
+      "w_tag", "l_tag",
+      "w_games_won", "l_games_won",
+      "w_seed", "l_seed")
     val setSchemaCSV = setSchema.mkString(",")
-    println(setSchemaCSV)
+    csvFile.println(setSchemaCSV)
+    // println(setSchemaCSV)
+
+    //METHOD - findTournamentData(input)
 
     val tourney = doc.select("div#title").first().text()
     val host = doc.select(".host").select("a").text()
-    val entrants = doc.select(".list .full-width").text().trim().split(" ")(0)
+    val date = findTourneyDate(host, bracketAddress)
+    val entrants = doc.select(".list .left-col").text().trim().split(" ")(0)
     // This returns the first element of a modified array of text.
     // It depends on Challonge being consistent with its organization of specific text.
+    var match_num: Int = 0
 
-
-    for  (set <- allSets) {
+    for (set <- allSets) {
 
       val setInfo = set.select(".participant-present").first()
       if (setInfo != null) {
@@ -43,49 +58,103 @@ object Main extends App {
         val isTopWinner = top.attr("class").contains("winner")
         val winnerHalf = if (isTopWinner) "top" else "bottom"
         val loserHalf = if (!isTopWinner) "top" else "bottom"
+        var wTag = set.select(".match_" + winnerHalf + "_half").select("span").text()
+        if (wTag.contains(",")) wTag = (wTag.split(",")(0) + ";" + wTag.split(",")(1))
+        var lTag = set.select(".match_" + loserHalf + "_half").select("span").text()
+        if (lTag.contains(",")) lTag = (lTag.split(",")(0) + ";" + lTag.split(",")(1))
+        val wGamesWon = set.select("." + winnerHalf + "_score").text()
+        val lGamesWon = set.select("." + loserHalf + "_score").text()
+        val wSeed = set.select("." + winnerHalf + "_seed").text()
+        val lSeed = set.select("." + loserHalf + "_seed").text()
 
         val setRow = Array(
           tourney,
           host,
+          date,
           entrants,
           match_num.toString,
           dataRound,
-          set.select(".match_" + winnerHalf + "_half").select("span").text(),
-          set.select(".match_" + loserHalf + "_half").select("span").text(),
-          set.select("." + winnerHalf + "_score").text(),
-          set.select("." + loserHalf + "_score").text(),
-          set.select("." + winnerHalf + "_seed").text(),
-          set.select("." + loserHalf + "_seed").text()
+          wTag,
+          lTag,
+          wGamesWon,
+          lGamesWon,
+          wSeed,
+          lSeed
         )
 
         val setRowCSV = setRow.mkString(",")
-
-
-        println(setRowCSV)
-        // println(match_num)
+        csvFile.println(setRowCSV)
+        // println(setRowCSV)
 
       }
+    } // ****End of for loop**** \\
+
+    csvFile.close()
+
+  }
+
+  def findTourneyDate(tourneyHost : String, tourneyAddress : String) = {
+    var pgNumber = 1
+    var tourneyDate = ""
+    val fullTourneyAddress = "http://challonge.com/" + tourneyAddress
+    // Goal: When the "address" string matches the web address of the relevant tournament,
+    // this value becomes non-null, the corresponding date is found, and the loop ends.
+
+    while (tourneyDate.equals("")) {
+      // println(pgNumber)
+
+      val append = if (pgNumber == 1) "" else "?page=" + pgNumber
+      val serviceD = url("http://challonge.com/users/" + tourneyHost + append)
+      val pageD = Http(serviceD OK as.String)
+      // Include a check to make sure the page exists?
+
+      val contentD = pageD()
+      val docD = Jsoup.parse(contentD)
+      val allTourneys = docD.select("tr").asScala
+
+      //print(allTourneys)
+
+      for (oneTourney <- allTourneys) {
+
+        val tourneyInfo = oneTourney.select("tr")
+        if (tourneyInfo != null) {
+          val address = tourneyInfo.select("td").select("a").attr("href")
+          val date : String = tourneyInfo.select(".hidden-phone").last().text()
+          if (address.equals(fullTourneyAddress)) tourneyDate = date
+          //println(tourneyDate)
+          //println("*****")
+        }
+      }
+
+      pgNumber += 1
 
     }
 
-    // ***Attempted to fnd the date; turns out to be pretty tricky. We'll come back to this later.***
+    tourneyDate
 
-    /* def FindStartDate (tourneyAddress: String) : String = {
-      import dispatch._, Defaults._
-      import dispatch.url
+  }
 
-      val logAddress = tourneyAddress + "/log"
+  gatherData("sappfe")
 
-      val svc2 = url(logAddress)
-      val pg2 = Http(svc2 OK as.String)
-
-      for (content <- pg2) {
-        val doc2 = Jsoup.parse(content)
-        val startDate = doc2.select()
-      }
-    } */
-
-
-   }
+  println("Done.")
 
 }
+
+
+
+// ****Attempted to fnd the date; turns out to be pretty tricky. We'll come back to this later.**** \\
+
+/* def FindStartDate (tourneyAddress: String) : String = {
+import dispatch._, Defaults._
+import dispatch.url
+
+val logAddress = tourneyAddress + "/log"
+
+val svc2 = url(logAddress)
+val pg2 = Http(svc2 OK as.String)
+
+for (content <- pg2) {
+  val doc2 = Jsoup.parse(content)
+  val startDate = doc2.select()
+}
+} */
